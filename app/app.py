3f6ac4e98fdc52a9cafe93afd0991a84e2d7ed38
Theo <58122097+tzebre@users.noreply.github.com>
2023-07-05 23:39:50 -0400

    trouver mieux que variable globale ?

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, g
 from jinja_maker import save
 
 app = Flask(__name__)
-rectangles = {}
 
 possible_args = {
     "Button": ["corner_radius", "border_width", "border_spacing", "fg_color", "hover_color", "border_color",
@@ -34,6 +33,74 @@ widget_list = [
     "Slider"
 ]
 
+x = 0
+y = 0
+height = 100
+width = 100
+all = {}
+
+
+def add_widget(widget):
+    name = widget.name
+    value = widget
+    all[name] = value
+
+
+def get_size():
+    return x, y, height, width
+
+
+def set_sizing(**kwargs):
+    global x, y, height, width
+    for arg in kwargs:
+        if arg == "x":
+            x = kwargs[arg]
+        elif arg == "y":
+            y = kwargs[arg]
+        elif arg == "height":
+            height = kwargs[arg]
+        elif arg == "width":
+            width = kwargs[arg]
+        else:
+            print("error")
+
+
+def get_position(name):
+    widget = all[name]
+    print(x, y, height)
+    old_x, old_y, h, w = widget.get_relative('x', 'y', 'height', 'width')
+    new_x = x + ((old_x * width) / 100)
+    new_y = y + ((old_y * height) / 100)
+    new_h = (h * height) / 100
+    new_w = (w * width) / 100
+
+    return {"x": new_x, "y": new_y, "height": new_h, "width": new_w}
+
+
+class Widget():
+    def __init__(self, name, coordinate, widget_type=None):
+        self.name = name
+        self.coordinate = coordinate
+        self.type = widget_type
+
+    def get_name(self):
+        return self.name
+
+    def get_relative(self, *args):
+        if args:
+            to_return = []
+            for request_arg in args:
+                try:
+                    to_return.append(self.coordinate[request_arg])
+                except:
+                    print(
+                        f"Warning: Exception occurred for argument :{request_arg}."
+                        f" Valid argument are: x, y, height, width")
+            return to_return
+        else:
+            return self.coordinate
+
+
 @app.route('/')
 def index():
     return render_template('index.html', drop_values=widget_list)
@@ -45,6 +112,27 @@ def save_all():
     # Process the rectangles data as needed
     # Save the rectangles in a dictionary or perform any other necessary actions
     save("/Users/theomathieu/Downloads", data)
+    return jsonify(success=True)
+
+
+@app.route('/new_widget', methods=['POST'])
+def new_widget():
+    data = request.json
+    test = Widget("test", data)
+    add_widget(test)
+    print(all)
+    print(get_size())
+    print("position_all", get_position('test'))
+    print("position_wid", test.get_relative())
+    print("relative", test.get_name(), test.get_relative("x", "y"))
+    return jsonify(success=True)
+
+
+@app.route('/resize', methods=['POST'])
+def resize():
+    data = request.json
+    set_sizing(**data)
+    print(get_size())
     return jsonify(success=True)
 
 
--- app/static/script.js
+++ app/static/script.js
@@ -34,46 +34,94 @@ window.onload = function () {
 
         canvas.style.left = offsetX + "px";
         canvas.style.top = offsetY + "px";
+
+        let res_dict = {
+            x: offsetX,
+            y: offsetY,
+            width: canvasWidth,
+            height: canvasHeight,
+        };
+        fetch('/resize', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(res_dict)
+        })
+            .then(response => {
+                // Handle the response from Flask
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
 
 
-window.addEventListener('resize', resizeCanvas);
+    window.addEventListener('resize', resizeCanvas);
 
 
-function startDrawing(event) {
-    isDrawing = true;
-    rect.startX = event.clientX - canvasRect.left;
-    rect.startY = event.clientY - canvasRect.top;
-}
-
-function drawRectangle(event) {
-    if (!isDrawing) return;
-
-    var x = event.clientX - canvasRect.left;
-    var y = event.clientY - canvasRect.top;
-
-    var width = x - rect.startX;
-    var height = y - rect.startY;
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.strokeRect(rect.startX, rect.startY, width, height);
-}
-
-function stopDrawing() {
-    isDrawing = false;
-    var width = event.clientX - rect.startX - canvasContainer.left;
-    var height = event.clientY - rect.startY - canvasContainer.top;
-    if (width < 0) {
-        width = -width
-        rect.startX = rect.startX - width
+    function startDrawing(event) {
+        isDrawing = true;
+        rect.startX = event.clientX - canvasRect.left;
+        rect.startY = event.clientY - canvasRect.top;
     }
-    if (height < 0) {
-        height = -height
-        rect.startY = rect.startY - height
+
+    function drawRectangle(event) {
+        if (!isDrawing) return;
+
+        let x = event.clientX - canvasRect.left;
+        let y = event.clientY - canvasRect.top;
+
+        let width = x - rect.startX;
+        let height = y - rect.startY;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.strokeRect(rect.startX, rect.startY, width, height);
     }
-}
 
+    function stopDrawing(event) {
+        isDrawing = false;
+        let x = event.clientX - canvasRect.left;
+        let y = event.clientY - canvasRect.top;
+        if (x < 0){
+            x = 0
+        }
+        if (y <0){
+            y = 0
+        }
+        let width = x - rect.startX;
+        let height = y - rect.startY;
+        if (width < 0) {
+            width = -width
+            rect.startX = rect.startX - width
+        }
+        if (height < 0) {
+            height = -height
+            rect.startY = rect.startY - height
+        }
+        console.log(rect.startX, rect.startY, height, width, canvas.width, canvas.height)
 
+        let res_dict = {
+            x: rect.startX / canvas.width * 100,
+            y: rect.startY / canvas.height * 100,
+            width: width / canvas.width * 100,
+            height: height / canvas.height * 100,
+        };
+        fetch('/new_widget', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(res_dict)
+        })
+            .then(response => {
+                // Handle the response from Flask
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+    }
 }
 
 
